cf5305b6f3bcb37f42ac5c444a41b5cb7d6c2f17
郑蒙 <zhengmeng@zms-rmbp.local>
2018-04-26 21:14:55 +0800

    china01

---
+++ leetcode/China01.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+#思路：两次循环判断两者想加是否等于taregt
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums)<2:
+            return
+        list1=[]
+        i=0
+        while i<len(nums):
+            j=i+1
+            while j<len(nums):
+                if nums[i]+nums[j]==target:
+                    list1.append(i)
+                    list1.append(j)
+                    return list1
+                    break
+                j+=1
+            i+=1
+        return
+                    
+            
+        
+        
